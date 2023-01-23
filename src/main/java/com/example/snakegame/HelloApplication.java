d6a68b97416ce32965e40175d45538626a755c2d
Mihail <mihail7enov@mail.ru>
2023-01-23 06:56:01 +0300

    menu developed

--- src/main/java/com/example/snakegame/HelloApplication.java
+++ src/main/java/com/example/snakegame/HelloApplication.java
@@ -1,16 +1,11 @@
 package com.example.snakegame;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ColorPicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
@@ -19,9 +14,12 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
 public class HelloApplication extends Application {
+    public static String NAME = "";
+    public static String DIFFICULT = "Easy";
+    public static Color COLOR = Color.GREEN;
+
     @Override
     public void start(Stage stage) {
 
@@ -30,38 +28,56 @@ public class HelloApplication extends Application {
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
-        grid.setBackground(Background.fill(Color.hsb(136.0,0.5,0.79)));
-        //grid.setGridLinesVisible(true);
-
-        final Text action = new Text();
-        grid.add(action, 0, 6);
+        Text action = new Text();
+        grid.add(action, 0, 10 );
+        action.setId("start");
         Label welcomeLabel = new Label("Hello there! This is my Snake game! ENJOY!");
         Label customizeLabel = new Label("Customize your Snake (if you want)");
         Label colorLabel = new Label("Choose color:");
         Label nameLabel = new Label("Enter snake name:");
+        Label chooseDifficult = new Label("Choose Difficult:");
+
+        ColorPicker colorPicker = new ColorPicker(Color.GREEN);
         TextField nameField = new TextField();
+        nameField.setMaxWidth(200.0);
         Button startButton = new Button("Start!");
+        ToggleGroup difficultGroup = new ToggleGroup();
+        RadioButton easyButton = new RadioButton("Easy");
+        RadioButton normalButton = new RadioButton("Normal");
+        RadioButton hardButton = new RadioButton("Hard");
+        easyButton.setToggleGroup(difficultGroup);
+        easyButton.setSelected(true);
+        normalButton.setToggleGroup(difficultGroup);
+        hardButton.setToggleGroup(difficultGroup);
         HBox hbStart = new HBox(10);
         hbStart.getChildren().add(startButton);
         startButton.setOnAction(e -> {
-            action.setFill(Color.FIREBRICK);
-            action.setText(nameField.getText());
+            NAME = nameField.getText();
+            DIFFICULT = ((RadioButton) difficultGroup.getSelectedToggle()).getText();
+            COLOR = colorPicker.getValue();
+            //action.setText("Your name is: " + NAME + "\nYour difficult: " + DIFFICULT + nameField.getWidth() + COLOR);
         });
-        ColorPicker colorPicker = new ColorPicker(Color.AQUA);
         welcomeLabel.setFont(Font.font("Times New Roman", FontWeight.BOLD, 20));
-        welcomeLabel.setFont(Font.font("Times New Roman", FontWeight.NORMAL, 20));
+        customizeLabel.setFont(Font.font("Times New Roman", FontWeight.NORMAL, 20));
         grid.add(welcomeLabel, 0, 0);
         grid.add(customizeLabel, 0, 1);
         grid.add(colorLabel, 0, 2);
         grid.add(colorPicker, 0, 2);
         grid.add(nameLabel, 0, 3);
         grid.add(nameField, 0, 4);
-        grid.add(hbStart,0, 5);
-        grid.add(startButton,0, 5);
+        grid.add(chooseDifficult, 0, 5);
+        grid.add(easyButton, 0, 6);
+        grid.add(normalButton, 0, 7);
+        grid.add(hardButton, 0, 8);
+        grid.add(hbStart,0, 9);
+        grid.add(startButton,0, 9);
 
         Scene scene = new Scene(grid, 600, 550);
         stage.setTitle("Snake The Game");
+        scene.getStylesheets().add(getClass().getResource("/styles/Menu.css").toExternalForm());
         stage.setScene(scene);
+        stage.setResizable(false);
+        stage.getIcons().add(new Image(getClass().getResource("/img/icon.jpg").toExternalForm()));
         stage.show();
     }
 
